426d40216b91d84af0301d3ffc45a3cbfe39ab1c
tanz <cauamelo2345@gmail.com>
2025-06-30 21:40:57 -0300

    fix uptime display

--- resources/scripts/components/dashboard/ServerRow.tsx
+++ resources/scripts/components/dashboard/ServerRow.tsx
@@ -92,7 +92,7 @@ export default ({ server, className }: { server: Server; className?: string }) =
 
     return (
         <StatusIndicatorBox as={Link} to={`/server/${server.id}`} className={className} $status={stats?.status}>
-            <div css={tw`flex items-center col-span-12 sm:col-span-5 lg:col-span-6`}>
+            <div css={tw`flex items-center col-span-15 sm:col-span-5 lg:col-span-6`}>
                 <div className={'icon mr-4'}>
                     <FontAwesomeIcon icon={faServer} />
                 </div>
@@ -117,7 +117,7 @@ export default ({ server, className }: { server: Server; className?: string }) =
                     </p>
                 </div>
             </div>
-            <div css={tw`hidden col-span-7 lg:col-span-4 sm:flex items-baseline justify-center`}>
+            <div css={tw`hidden col-span-7 lg:col-span-7 sm:flex items-baseline justify-center`}>
                 {!stats || isSuspended ? (
                     isSuspended ? (
                         <div css={tw`flex-1 text-center`}>
