9350e113b423526e4a26b72fb1232fe09acec91f
tanz <cauamelo2345@gmail.com>
2025-06-30 21:43:28 -0300

    fix the fix

--- resources/scripts/components/dashboard/ServerRow.tsx
+++ resources/scripts/components/dashboard/ServerRow.tsx
@@ -92,7 +92,7 @@ export default ({ server, className }: { server: Server; className?: string }) =
 
     return (
         <StatusIndicatorBox as={Link} to={`/server/${server.id}`} className={className} $status={stats?.status}>
-            <div css={tw`flex items-center col-span-15 sm:col-span-5 lg:col-span-6`}>
+            <div style={{ gridTemplateColumns: 'repeat(15, minmax(0px, 1fr))' }} css={tw`flex items-center sm:col-span-5 lg:col-span-6`}>
                 <div className={'icon mr-4'}>
                     <FontAwesomeIcon icon={faServer} />
                 </div>
